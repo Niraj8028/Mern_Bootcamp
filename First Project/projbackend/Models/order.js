7d92fbe1993f987259349e489f5812625a0a2cec
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-07 14:44:08 +0530

    order schema created

---
+++ First Project/projbackend/Models/order.js
@@ -0,0 +1,32 @@
+const mongoose= require("mongoose")
+const {ObjectId}= mongoose.Schema;
+
+const ProductCartSchema= new mongoose.Schema({
+    product:{
+        type: ObjectId,
+        ref:"product"
+    },
+    name:String,
+    count: Number,
+    price: Number
+}, {timestamps: true})
+
+const ProductCart= mongoose.model("ProductCart", ProductCartSchema);
+
+const OrderSchema= new mongoose.Schema({
+    product: [productCartSchema],
+    transaction_id:{},
+    amount:{
+        type: Number
+    },
+    address: String,
+    updated: Date,
+    user:{
+        type: ObjectId,
+        ref:"user"
+    }
+
+},{timestamps:true})
+
+const Order= mongoose.model("Order",OrderSchema);
+module.exports={Order, ProductCart}
