b579e9345bfe5884202d9fe52825bec37c11f34f
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-09 22:40:05 +0530

    signout routes defined

--- First Project/projbackend/app.js
+++ First Project/projbackend/app.js
@@ -3,6 +3,10 @@ require("dotenv").config();
 const mongoose = require('mongoose');
 const express= require("express")
 const app=express();
+const bodyParser=require('body-parser')
+const cookieParser=require('cookie-parser')
+const cors=require('cors')
+const authRoutes=require('./Routes/auth')
 
 mongoose.connect(process.env.DATABASE, 
     {useNewUrlParser:true,
@@ -11,6 +15,11 @@ mongoose.connect(process.env.DATABASE,
 }).then(()=>{
     console.log("DATABASE CONNECTED")
 });
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use("/api",authRoutes);
+
 const port=8000;
 
 app.listen(port,()=>{
