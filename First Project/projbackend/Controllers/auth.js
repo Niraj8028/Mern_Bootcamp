748074243ad95ae3ee0c204682c43bcc1b41455a
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-12 20:00:59 +0530

    Isadmin and isAuthenticated defined

--- First Project/projbackend/Controllers/auth.js
+++ First Project/projbackend/Controllers/auth.js
@@ -68,3 +68,22 @@ exports.isSignedIn=expressJwt({
     secret:process.env.SECRET,
     userProperty:"auth"
 })
+
+exports.isAuthenticated=(req,res,next)=>{
+    let checker= req.profile && req.auth && req.auth._id===req.profile._id
+    if(!checker){
+        return res.status(403).json({
+            error:"Access Denied"
+        })
+    }
+    next();
+}
+
+exports.isAdmin= (req,res,next)=>{
+    if(req.profile.role===0){
+     return res.status(403).json({
+        error:"You are not an Imposter"
+     })
+    }
+    next();
+}
