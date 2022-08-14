bd05f6a8da5a39ad3aaf6d4151bf4679efb1834d
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-14 23:13:53 +0530

    getAllUsers done

--- First Project/projbackend/Controllers/user.js
+++ First Project/projbackend/Controllers/user.js
@@ -16,6 +16,18 @@ exports.getUserById=(req,res,next,id)=>{
 exports.getUser=(req,res)=>{   
     req.profile.salt=undefined;
     req.profile.encrypt_password=undefined;
-    
+    req.profile.updatedAt=undefined;
+    req.profile.createdAt=undefined;
     return res.json(req.profile)
+}
+
+exports.getAllUsers=(req,res)=>{
+    User.find().exec((err,users)=>{
+        if(err || !users){
+            return res.status(400).json({
+                error:"No user was found"
+            })
+        }
+        res.json(users)
+    })
 }
--- First Project/projbackend/Routes/user.js
+++ First Project/projbackend/Routes/user.js
@@ -1,11 +1,11 @@
 const express= require("express");
 const router=express.Router();
-const { getUserById, getUser } = require("../Controllers/user");
+const { getUserById, getUser, getAllUsers } = require("../Controllers/user");
 const {isSignedIn , isAdmin , isAuthenticated}=require("../Controllers/auth")
 
 router.param("userId",getUserById)
 
 router.get("/user/:userId",isSignedIn,isAuthenticated,getUser)
-
+router.get("/users",getAllUsers)
 
 module.exports=router;
