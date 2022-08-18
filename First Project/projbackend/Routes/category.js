e811b35301755809d2d4d4a4292dd6737e600bd5
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-18 23:49:42 +0530

    create category done

--- First Project/projbackend/Routes/category.js
+++ First Project/projbackend/Routes/category.js
@@ -1,7 +1,7 @@
 const express=require("express")
 const router=express.Router();
 
-const {getCategoryById}=require('../Controllers/category')
+const {getCategoryById, createCategory}=require('../Controllers/category')
 const {getUserById}=require('../Controllers/user')
 const {isAuthenticated,isSignedIn,isAdmin}=require('../Controllers/auth')
 
@@ -9,4 +9,5 @@ const {isAuthenticated,isSignedIn,isAdmin}=require('../Controllers/auth')
 router.param("categoryId",getCategoryById)
 router.param("userId",getUserById)
 
+router.post("/category/create/:userId",isSignedIn,isAuthenticated,isAdmin,createCategory)
 module.exports=router;
