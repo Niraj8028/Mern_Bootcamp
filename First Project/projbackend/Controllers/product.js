6dc6bd38711489c8936126b7c364f68c097c886a
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-22 12:55:43 +0530

    product saved as a form

--- First Project/projbackend/Controllers/product.js
+++ First Project/projbackend/Controllers/product.js
@@ -1,5 +1,7 @@
 const Product=require("../Models/product")
-
+const formidable=require('formidable')
+const _=require("lodash")
+const fs=require("fs")
 
 exports.getProductById=(req,res,next,id)=>{
     Product.findById(id)
@@ -13,5 +15,42 @@ exports.getProductById=(req,res,next,id)=>{
         req.product=product;
         res.json(product);
         next()
+    })
+}
+
+exports.createProduct=(req,res)=>{
+    let form=new formidable.IncomingForm();
+
+    form.keepExtensions=true;
+
+    form.parse(req,(err,fields,file)=>{
+        if(err){
+            return res.status(400).json({
+                error:"There is some error in file"
+            })
+        }
+
+        const product= new Product(fields);
+
+        if(file.photo){
+            if(file.photo.size > 3000000){
+                return res.status(400).json({
+                    error:"file size is too large"
+                })
+            }
+            product.photo.data=fs.readFileSync(file.photo.path)
+            product.photo.contentType= file.photo.type;
+        }
+
+        product.save((err,product)=>{
+            if(err){
+                return res.status(400).json({
+                    error:"Saving Product in database failed"
+                })
+            }
+            res.json({product})
+        })
+
+
     })
 }
--- First Project/projbackend/Routes/product.js
+++ First Project/projbackend/Routes/product.js
@@ -2,7 +2,7 @@ const express=require('express');
 const { getProductById } = require('../Controllers/product');
 const router=express.Router();
 
-const {}=require('../Controllers/auth')
+const { isSignedIn, isAuthenticated, isAdmin }=require('../Controllers/auth')
 const { getUserById }=require('../Controllers/user')
 const {}=require('../Controllers/category')
 const {getProductById}=require('../Controllers/product')
@@ -10,5 +10,7 @@ const {getProductById}=require('../Controllers/product')
 
 router.param("productId",getProductById);
 router.param("userId",getUserById)
+
+router.post("product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct)
 
 module.exports=router;
