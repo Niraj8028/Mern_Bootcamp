e2df2d2495dac9819b33aaf0570e313b26090752
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-29 23:00:08 +0530

    signup method connected with backend

--- First Project/projfrontend/src/user/Signup.js
+++ First Project/projfrontend/src/user/Signup.js
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Base from '../core/Base'
+import { signup } from '../auth/helper';
 
-const signupForm=()=>{
+const signup=()=>{
+  const[values, setValues]=useState({
+    name:"",
+    email:"",
+    password:"",
+    error:"",
+    success:false
+  });
+
+  const{name,email,password,error,success}=values;
+
+  const handleChange=name=>event=>{
+    setValues({...values, error:values, [name]:event.target.value})
+  }
+  const handleSubmit=event=>{
+    event.preventDefault();
+    setValues({...values, error:false})
+    signup({name,email,password})
+    .then(data=>{
+      if(data.error){
+        setValues({...values,error:data.error,success:false})
+      }
+      else{
+        setValues({...values,name:"",email:"",password:"",error:"",success:true})
+      }
+
+    })
+    .catch(console.log("error in signup"));
+  };
+  const signupForm=()=>{
  
     return(
       <div className='row'>
@@ -9,23 +39,25 @@ const signupForm=()=>{
           <form>
             <div className='form-group'>
               <label className='text-light'>Name</label>
-              <input className='form-control' type="text"/>
+              <input className='form-control' onChange={handleChange("name")} type="text"/>
             </div>
             <div className='form-group'>
               <label className='text-light'>Email</label>
-              <input className='form-control' type="email"/>
+              <input className='form-control' onChange={handleChange("email")} type="email"/>
             </div> 
             <div className='form-group'>
               <label className='text-light'>Password</label>
-              <input className='form-control' type="password"/>
+              <input className='form-control' onChange={handleChange("password")} type="password"/>
             </div>
-            <button className='btn mt-3 btn-success btn-block'>Submit</button>
+            <button className='btn mt-3 btn-success btn-block' onClick={handleSubmit()}>Submit</button>
           </form>
   
         </div>
       </div>
     )
-  }
+  } 
+}
+  
 
 
 function Signup() {
