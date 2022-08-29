5ff70bcc0d2ad0bad399a4faece0d3f6c450dc15
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-29 21:55:32 +0530

    signin method created

--- First Project/projfrontend/src/auth/helper/index.js
+++ First Project/projfrontend/src/auth/helper/index.js
@@ -16,3 +16,21 @@ export const signup=user=>{
     
     .catch(err=>console.log(err));
 };
+
+
+export const signin=user=>{
+    return fetch(`${API}/signin`, {
+        method:"POST",
+        headers: {
+            Accept: "application/json",
+            "content-type":"application/json"
+        },
+        body:JSON.stringify(user)
+        
+    })
+    .then(response=>{
+        return response.json();
+    })
+    
+    .catch(err=>console.log(err));
+};
