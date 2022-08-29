acca6de7d07fcde3f34f0c661f59c2d2761794d5
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-08-29 23:07:59 +0530

    signup form bugs fixed

--- First Project/projfrontend/src/user/Signup.js
+++ First Project/projfrontend/src/user/Signup.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Base from '../core/Base'
 import { signup } from '../auth/helper';
 
-const signup=()=>{
+const Signup=()=>{
   const[values, setValues]=useState({
     name:"",
     email:"",
@@ -13,10 +13,10 @@ const signup=()=>{
 
   const{name,email,password,error,success}=values;
 
-  const handleChange=name=>event=>{
-    setValues({...values, error:values, [name]:event.target.value})
+  const handleChange=(name)=>event=>{
+    setValues({...values, error:false, [name]:event.target.value})
   }
-  const handleSubmit=event=>{
+  const handleSubmit=(event)=>{
     event.preventDefault();
     setValues({...values, error:false})
     signup({name,email,password})
@@ -39,35 +39,29 @@ const signup=()=>{
           <form>
             <div className='form-group'>
               <label className='text-light'>Name</label>
-              <input className='form-control' onChange={handleChange("name")} type="text"/>
+              <input className='form-control' onChange={handleChange("name")} type="text" value={name}/>
             </div>
             <div className='form-group'>
               <label className='text-light'>Email</label>
-              <input className='form-control' onChange={handleChange("email")} type="email"/>
+              <input className='form-control' onChange={handleChange("email")} type="email" value={email}/>
             </div> 
             <div className='form-group'>
               <label className='text-light'>Password</label>
-              <input className='form-control' onChange={handleChange("password")} type="password"/>
+              <input className='form-control' onChange={handleChange("password")} type="password" value={password}/>
             </div>
-            <button className='btn mt-3 btn-success btn-block' onClick={handleSubmit()}>Submit</button>
+            <button className='btn mt-3 btn-success btn-block' onClick={handleSubmit}>Submit</button>
           </form>
   
         </div>
       </div>
     )
   } 
-}
-  
 
-
-function Signup() {
   return (
     <Base title='A signout Page' description='A page for user signup'>
-        
-
         {signupForm()}
     </Base>
-  )
-}
+  );
+  };
 
 export default Signup
