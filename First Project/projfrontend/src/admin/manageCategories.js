8040cd1b4db25789bfb1f58db85808cbf6773c5a
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-06 20:57:55 +0530

    manage categories

---
+++ First Project/projfrontend/src/admin/manageCategories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Base from '../core/Base'
+
+
+function manageCategories() {
+  return (
+    <Base>
+      <h1>Assignment</h1>
+    </Base>
+  )
+}
+
+export default manageCategories
