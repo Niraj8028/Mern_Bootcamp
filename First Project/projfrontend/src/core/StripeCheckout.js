fa4963c4d5a1df84adeb6c63aeb7c8b939c91776
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-17 04:15:21 +0530

    stripe checkout popup integrated

--- First Project/projfrontend/src/core/StripeCheckout.js
+++ First Project/projfrontend/src/core/StripeCheckout.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import { isAuthenticated } from '../auth/helper'
 import { loadCart } from './helper/CartHelper'
+import StripeCheckoutButton from 'react-stripe-checkout'
 
 function StripeCheckout({products,
                         setReload=f=>f,
@@ -26,10 +27,22 @@ function StripeCheckout({products,
       })
       return amount;
     }
+    const makePayment=(token)=>{
+      // 
+    }
 
     const showCheckout=()=>{
       return isAuthenticated()?(
-        <button className='btn btn-success'>Pay with Stripe</button>
+        <StripeCheckoutButton
+          stripeKey=""
+          token={makePayment}
+          amount={getTotal()*100} 
+          name="Buy Tshirts"
+          shippingAddress
+          billingAddress
+        >
+          <button className='btn btn-success'>Pay with Stripe</button>
+        </StripeCheckoutButton>
       ):(<Link to="/signin">
           <button className='btn btn-warning'>Signin</button>
         </Link>)
@@ -40,6 +53,7 @@ function StripeCheckout({products,
     <div>
       <h3 className='text-white'>Checkout amount is {getTotal()}</h3>
       {showCheckout()}
+      
     </div>
   )
 }
