2698ce986076451d4c4b4c5e184fae0632614481
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-09-16 23:15:44 +0530

    remove from cart

--- First Project/projfrontend/src/core/helper/CartHelper.js
+++ First Project/projfrontend/src/core/helper/CartHelper.js
@@ -1,7 +1,26 @@
+import { type } from "@testing-library/user-event/dist/type";
+
 export const loadcart=()=>{
     if(typeof window !== undefined){
         if(localStorage.getItem("cart")){
             return JSON.parse(localStorage.getItem("cart"));
         }
     }
+}
+
+export const removeFromCart=(productId)=>{
+    let cart=[]
+    if(typeof window !==undefined){
+        if(localStorage.getItem("cart")){
+            cart=JSON.getItem(localStorage.getItem("cart"))
+        }
+        cart.map((product,i)=>{
+            if(product._id===productId){
+                cart.splice(i,1);
+            }
+        })
+        localStorage.setItem("cart",JSON.stringify("cart"));
+    }
+    return cart;
+
 }
